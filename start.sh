ecdd8a9531740c3f2bc8a460b880275fc5bbb496
Marsrois <mars@rois15.ru>
2025-04-20 22:44:29 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,3 +2,7 @@
 # Указываем, на каком порту будет работать приложение
 export PORT=8000
 python3 encar_bot.py
+chmod +x start.sh
+git add start.sh
+git commit -m "Make start.sh executable"
+git push
